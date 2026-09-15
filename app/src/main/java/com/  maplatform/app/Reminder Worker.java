5972b1ebc8c252c63de79f9c9caabaec35abfdf9
mezo2021 <88048816+mezo2021@users.noreply.github.com>
2026-09-15 05:47:44 +0300

    Add ReminderWorker class for background tasks

---
+++ app/src/main/java/com/  maplatform/app/Reminder Worker.java
@@ -0,0 +1,23 @@
+package com.maplatform.app;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class ReminderWorker extends Worker {
+
+    public ReminderWorker(@NonNull Context context, @NonNull WorkerParameters params) {
+        super(context, params);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        try {
+            NotificationHelper.showNow(getApplicationContext());
+        } catch (Exception ignored) {}
+        return Result.success();
+    }
+}
